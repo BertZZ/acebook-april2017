dd528ccd36fc41b5361c3c756c9f8149ddda00f1
Timothy Robinson <timmyrobby@gmail.com>
2017-06-03 13:57:14 +0100

    Keeps sign-up background off posts page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,7 @@ ready(function() {
 });
 
 ready(function() {
-var slides = document.querySelectorAll('#slides .slide');
+var slides = document.querySelectorAll('.slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,3500);
 
